3c7250c7375e463f192b09e34227260ff8e67f12
rwbxd <will.bryan99@gmail.com>
2024-02-05 13:18:49 -0600

    Refactor to make more readable and consistent

--- code/24Part2.py
+++ code/24Part2.py
@@ -1,25 +1,14 @@
 from sympy import Symbol, solve_poly_system
+from typing import NamedTuple
 
-class Vec3D():
-    def __init__(self, x: int, y: int, z: int):
-        self.x = x
-        self.y = y
-        self.z = z
-    
-    def __add__(self, o):
-        if isinstance(o, Vec3D):
-            return Vec3D(self.x + o.x, self.y + o.y, self.z + o.z)
-        
-    def __mul__(self, o):
-        if isinstance(o, int):
-            return Vec3D(self.x * o, self.y * o, self.z * o)
-        
-    def __str__(self):
-        return f"<{self.x}, {self.y}, {self.z}>"
+class Vec3D(NamedTuple):
+    x: int
+    y: int
+    z: int
 
-# Position and velocity of given hailstones
-position_vectors: list[Vec3D] = []
-velocity_vectors: list[Vec3D] = []
+class Hailstone(NamedTuple):
+    position_vector: Vec3D
+    velocity_vector: Vec3D
 
 # We are looking for a hailstone with position P0 = <PX0, PY0, PZ0> and velocity V0 = <VX0, VY0, VZ0>
 # that intercepts each hailstone with position PI = <PXI, PYI, PZI> and velocity VI = <VXI, VYI, VZI>
@@ -42,36 +31,42 @@ velocity_vectors: list[Vec3D] = []
 # we introduce one new unknown (TK) and three new equations, for
 # a total of 9 equations and 9 unknowns, which we can plug into a linear solver.
 
+# Hailstones given in the input
 # Any 3 hailstones will suffice, but for simplicity I used the first three
-# First hailstone
-position_vectors.append(Vec3D(327367788702047, 294752664325632, 162080199489440))
-velocity_vectors.append(Vec3D(20, 51, 36))
-# Second hailstone
-position_vectors.append(Vec3D(349323332347395, 429135322811787, 397812423558610))
-velocity_vectors.append(Vec3D(-96, -480, -782))
-# Third hailstone
-position_vectors.append(Vec3D(342928768632768, 275572250031810, 310926883862869))
-velocity_vectors.append(Vec3D(-69, 104, -510))
+hailstones: list[Hailstone] = [
+    Hailstone( # First hailstone
+        position_vector=Vec3D(327367788702047, 294752664325632, 162080199489440),
+        velocity_vector=Vec3D(20, 51, 36)
+    ),
+    Hailstone( # Second hailstone
+        position_vector=Vec3D(349323332347395, 429135322811787, 397812423558610),
+        velocity_vector=Vec3D(-96, -480, -782)
+    ),
+    Hailstone( # Third hailstone
+        position_vector=Vec3D(342928768632768, 275572250031810, 310926883862869),
+        velocity_vector=Vec3D(-69, 104, -510)
+    ),
+]
 
 # Add intial unknown variables
-PX0 = Symbol('PX0')
-PY0 = Symbol('PY0')
-PZ0 = Symbol('PZ0')
-VX0 = Symbol('VX0')
-VY0 = Symbol('VY0')
-VZ0 = Symbol('VZ0')
+PX0 = Symbol('PX0') # Initial X position
+PY0 = Symbol('PY0') # Initial Y position
+PZ0 = Symbol('PZ0') # Initial Z position
+VX0 = Symbol('VX0') # X velocity
+VY0 = Symbol('VY0') # Y velocity
+VZ0 = Symbol('VZ0') # Z velocity
 symbols = [PX0, PY0, PZ0, VX0, VY0, VZ0]
 
 equations = []
-for i, (p, v) in enumerate(zip(position_vectors, velocity_vectors)):
-    # Create impact time unknown variable
-    t = Symbol(f'T{i}')
-    symbols.append(t)
+for i, (PI, VI) in enumerate(hailstones, start=1):
+    # Create unknown variable for impact time
+    TI = Symbol(f'T{i}')
+    symbols.append(TI)
 
     # Create equations for the impact of P0, V0 at time TI with PI and VI
-    equations.append((p.x + t * (v.x - VX0) - PX0)) # Add X equation
-    equations.append((p.y + t * (v.y - VY0) - PY0)) # Add Y equation
-    equations.append((p.z + t * (v.z - VZ0) - PZ0)) # Add Z equation
+    equations.append((PI.x + TI * (VI.x - VX0) - PX0)) # Add X equation
+    equations.append((PI.y + TI * (VI.y - VY0) - PY0)) # Add Y equation
+    equations.append((PI.z + TI * (VI.z - VZ0) - PZ0)) # Add Z equation
 
 # Solve
 result = solve_poly_system(equations, *symbols)
